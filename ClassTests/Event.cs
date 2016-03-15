bca1aa8547aee3e090f5b27ab8dad96186ee72dc
Allan Wiener <awiener1@users.noreply.github.com>
2016-03-15 17:28:37 -0400

    documented Event class

--- ClassTests/Event.cs
+++ ClassTests/Event.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/**
+ * @file
+ * @author Allan Wiener
+ * 
+ * @section DESCRIPTION
+ * 
+ * The Event class defines one single event
+ * occurring on the timeline.
+ * 
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +17,44 @@ namespace Span
 {
     class Event
     {
-
-        public Event(bool a_isTask, string a_name, List<Occurrence> a_manualOccurrences, List<Period> a_allRules, string a_loc, List<string> a_categories, AlarmSettings a_alarms, string a_desc)
+        /**
+         * Creates a new Event from the specified information.
+         * 
+         * @param a_isTask specifies if the Event is a task.
+         * However, if it is a task, please use a TaskEvent object
+         * instead.
+         * 
+         * @param a_name a name or title for the event. The name
+         * can be changed at any time, as the event is internally
+         * identified with an Id. This string must be single-line.
+         * 
+         * @param a_manualOccurrences a list of Occurrences defined
+         * manually. These will occur only once on the timeline.
+         * 
+         * @param a_allRules a list of Period objects specifying
+         * periodic Occurrences for the Event, which may occur
+         * multiple times on the timeline.
+         * 
+         * @param a_loc a place or location for the event. This
+         * parameter is mandatory, but will be ignored if it is
+         * set to a blank string. This string must be single-line.
+         * 
+         * @param a_categories a list of ids for Categories to which
+         * this Event belongs. Index 0 is the PrimaryCategory and
+         * the rest are SecondaryCategories.
+         * 
+         * @param a_alarms an AlarmSettings object indicating when
+         * alarms should go off for each Occurrence of this Event.
+         * Note: the alarm settings are the same for every Occurrence,
+         * but they can be changed at any time.
+         * 
+         * @param a_desc a description for the Event. This string may
+         * be multi-line.
+         * 
+         * @date March 14, 2016
+         */
+        public Event(bool a_isTask, string a_name, List<Occurrence> a_manualOccurrences, List<Period> a_allRules, 
+            string a_loc, List<string> a_categories, AlarmSettings a_alarms, string a_desc)
         {
             m_numId = num++;
             m_id = "e" + m_numId.ToString("x8");
@@ -24,6 +70,19 @@ namespace Span
             All.Add(m_id, this);
         }
 
+        /**
+         * Returns a list of the Ids of all Events overlapping this one
+         * on the timeline.
+         * 
+         * @return a Tuple containing a uint value and a List of
+         * strings. The uint value specifies the number of
+         * overlapping events. If the uint value is 0, the
+         * list should be empty. If the uint value is greater than
+         * 1, the list includes that many event Ids. If the uint
+         * is equal to 1, the list includes the event Id at index
+         * 0, and the ids of the overlapping Occurrences for this
+         * Event and the other Event at indices 1 and 2, respectively.
+         */
         public Tuple<uint, List<string>> getOverlapping()
         {
             List<string> overlapping = new List<string>();
@@ -74,25 +133,44 @@ namespace Span
             return new Tuple<uint, List<string>>((uint)overlapping.Count, overlapping);
         }
 
-        
+        /**
+         * Gets or sets a name or title for the event. 
+         * This string must be single-line.
+         */
         public string Name 
         {
             get { return m_name; }
             set { m_name = value; }
         }
 
+        /**
+         * Specifies or sets whether this Event exists. True by
+         * default. If false, the Event is removed from the timeline
+         * and is not saved when the program closes, effectively and
+         * safely deleting it from existence.
+         */
         public bool Exists
         {
             get { return m_exists; }
             set { m_exists = value; }
         }
 
+        /**
+         * Gets or sets a list of Occurrences defined
+         * manually. These will occur only once on the timeline.
+         */
         public List<Occurrence> ManualOccurrences
         {
             get { return m_manualOccurrences; }
             set { m_manualOccurrences = value; }
         }
 
+        /**
+         * Updates the list of all Occurrences based on the list of
+         * ManualOccurrences and the list of Rules.
+         * 
+         * @date March 14, 2016
+         */
         //TODO: make this work with periods that are changed into
         //manual occurrences
         private void updateOccurrences()
@@ -119,6 +197,9 @@ namespace Span
             }
         }
 
+        /**
+         * Gets the list of all Occurrences of this Event.
+         */
         public List<Occurrence> Occurrences
         {
             get 
@@ -128,24 +209,45 @@ namespace Span
             }
         }
 
+        /**
+         * Gets or sets the list of all Period objects
+         * that define the periodic Occurrences of this
+         * Event.
+         */
         public List<Period> Rules
         {
             get { return m_allRules; }
             set { m_allRules = value; }
         }
 
+        /**
+         * Gets or sets a place or location for the event. This
+         * property will be ignored if it is
+         * set to a blank string. This string must be single-line.
+         */
         public string Location
         {
             get { return m_loc; }
             set { m_loc = value; }
         }
 
+        /**
+         * Gets or sets a list of ids for Categories to which
+         * this Event belongs. Index 0 is the PrimaryCategory and
+         * the rest are SecondaryCategories.
+         */
         public List<string> Categories
         {
             get { return m_categories; }
             set { m_categories = value; }
         }
 
+        /**
+         * Gets or sets the primary Category to which this Event
+         * belongs. If the primary category is set to one that
+         * is already a secondary category, the existing primary
+         * category and the desired one will switch.
+         */
         public string PrimaryCategory
         {
             get { return Categories[0]; }
@@ -166,6 +268,16 @@ namespace Span
             }
         }
 
+        /**
+         * Gets or sets the list of secondary Categories to
+         * which this Event belongs. The existing primary
+         * Category cannot be added to this list, and must be
+         * changed first.
+         * 
+         * @throws ArgumentException if the primary category
+         * is included in the list of secondary categories
+         * to be set.
+         */
         public List<string> SecondaryCategories
         {
             get 
@@ -182,23 +294,48 @@ namespace Span
                 }
                 List<string> copy = new List<string>(value);
                 copy.Insert(0, PrimaryCategory);
+                Categories = copy;
             }
         }
 
+        /**
+         * Gets or sets an AlarmSettings object indicating when
+         * alarms should go off for each Occurrence of this Event.
+         * Note: the alarm settings are the same for every Occurrence,
+         * but they can be changed at any time.
+         */
         public AlarmSettings Alarms
         {
             get { return m_alarms; }
             set { m_alarms = value; }
         }
 
+        /**
+         * Gets or sets a description for the Event. This string may
+         * be multi-line.
+         */
         public string Description
         {
             get { return m_desc; }
             set { m_desc = value; }
         }
 
+        /**
+         * Gets the id of the Event.
+         * 
+         * The id is written in the form of a string starting
+         * with the letter 'e', followed by a 32-bit unsigned
+         * integer in hexadecimal (all lowercase). This allows
+         * the event to be looked up easily.
+         * 
+         * Currently, the integer portion of the id is
+         * simply equal to the event's number.
+         */
         public string Id { get { return m_id; } }
 
+        /**
+         * Gets a Dictionary containing all Events by Id.
+         */
         public static Dictionary<string, Event> All { get { return all; } }
 
 
